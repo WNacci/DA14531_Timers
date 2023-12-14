9c359117572f93bbaf02b59739df360d6d374d38
William Nacci <wnacci@gmail.com>
2023-12-13 21:24:41 -0600

    Retried, same error... :(

--- src/user_app.c
+++ src/user_app.c
@@ -12,6 +12,117 @@
 #include <ble_handlers.h>
 #include <debug.h>
 
+//Timer Section#################
+#include "app_api.h"
+#include "arch_system.h"
+#include "timer1.h"
+
+/*
+ * TYPE DEFINITIONS
+ ****************************************************************************************
+ */
+ 
+// Timer1 counting configuration structure
+static timer1_count_options_t timer1_config =
+{	
+	/*Timer1 input clock*/
+	.input_clk 	= 	(tim1_clk_src_t)INPUT_CLK,
+	
+	/*Timer1 free run off*/
+	.free_run 	= 	FREE_RUN,
+	
+	/*Timer1 IRQ mask*/
+	.irq_mask	= 	INTERRUPT_MASK_TMR,
+	
+	/*Timer1 count direction*/
+	.count_dir	= 	COUNT_DIRECTION,
+	
+	/*Timer1 reload value*/
+	.reload_val	= 	RELOAD_VALUE
+};
+
+
+
+/*
+ * GLOBAL VARIABLE DEFINITIONS
+ ****************************************************************************************
+ */
+// Retained variables
+
+uint8_t timer1_cnt_ovf                          __SECTION_ZERO("retention_mem_area0"); //@RETENTION MEMORY
+bool led_state                                  __SECTION_ZERO("retention_mem_area0"); //@RETENTION MEMORY
+//extern rcx_time_data_t rcx_time_data;
+
+/*
+ * FUNCTION DEFINITIONS
+ ****************************************************************************************
+*/
+
+
+static void timer1_overflow(void);
+
+//void user_on_connection(uint8_t connection_idx, struct gapc_connection_req_ind const *param)
+//{
+//    default_app_on_connection(connection_idx, param);
+//}
+
+//void user_on_disconnect( struct gapc_disconnect_ind const *param )
+//{
+//    default_app_on_disconnect(param);
+//}
+//
+//void user_app_adv_start(void)
+//{
+//
+//    default_advertise_operation();  
+//}
+
+void timer1_initialize(void)
+{
+    /*Enable PD_TIM power domain*/
+	SetBits16(PMU_CTRL_REG, TIM_SLEEP, 0);                  // Enable the PD_TIM
+    timer1_count_config(&timer1_config, timer1_overflow);   // Set the capture counting configurations for the timer
+
+    printf("Timer 1 is initialized \n\r") ;
+}
+
+static void user_timer_wakeup_ble(void)
+{
+    printf("BLE is awake via timer 1\n\r");
+}
+
+static void toggle_led(void)
+{
+    if(led_state)
+    {
+        GPIO_SetInactive(GPIO_LED_PORT, GPIO_LED_PIN);
+        led_state = false;
+    }
+    else
+    {
+        GPIO_SetActive(GPIO_LED_PORT, GPIO_LED_PIN);
+        led_state = true;
+    }
+}
+
+static void timer1_overflow(void)
+{
+    if (timer1_cnt_ovf == TIMER1_OVF_COUNTER)
+    {
+        toggle_led();
+        
+        timer1_cnt_ovf = 0;
+        /* Wake up the device and print out the measured time */
+        arch_ble_force_wakeup();                // Force the BLE to wake up
+        app_easy_wakeup_set(user_timer_wakeup_ble);
+        app_easy_wakeup();                      // Send the message to print
+    }
+    
+    timer1_cnt_ovf++;
+}
+
+//##############################
+
 /*
 This function is called as a callback by system arch, after peripheral init, and app init, but before giving away to the kernel and main loop
 */
@@ -33,6 +144,12 @@ void app_on_init(void)
     printf("stack: 0x%08lX\r\n", initial_sp);
     printf("heap: 0x%08lX (0x%04X)\r\n", heap_base, (uint16_t)heap_limit);
 	printf("Advertized Name: %s\n",USER_DEVICE_NAME);
+
+	//Timer stuff
+    timer1_initialize();
+	timer1_clear_all_events();
+    timer1_enable_irq();
+    timer1_start();
 }
 
 void app_resume_from_sleep(void)
